f0178509faa75ab0599ff19f1872f1f2e580771b
headshot2017 <nou@jm>
2023-01-06 16:10:37 -0400

    many changes to main.cpp
    * separated audio and d3d9 functions from main.
    * on init, mario's texture's red and blue colors are swapped to fix incorrect colors.
    * don't load twice.
    * added more event callbacks.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include "plugin.h"
 #include "CHud.h"
 
-#include "audio.h"
 extern "C" {
     #include <libsm64.h>
     #include <decomp/include/PR/ultratypes.h>
     #include <decomp/include/audio_defines.h>
 }
 
+#include "audio.h"
+#include "d3d9_funcs.h"
+
 using namespace plugin;
 
-bool loaded = false;
+bool loaded;
 std::string message;
+uint8_t* marioTexture;
 
 class sm64_san_andreas {
 public:
     static void init()
     {
+        if (loaded) return;
+
         std::ifstream file("sm64.us.z64", std::ios::ate | std::ios::binary);
 
         if (!file)
@@ -36,21 +41,43 @@ public:
             file.close();
 
             // Mario texture is 704x64 RGBA
-            uint8_t* mario_texture = (uint8_t*)malloc(4 * SM64_TEXTURE_WIDTH * SM64_TEXTURE_HEIGHT);
+            marioTexture = new uint8_t[4 * SM64_TEXTURE_WIDTH * SM64_TEXTURE_HEIGHT];
 
             // load libsm64
-            sm64_global_init(romBuffer, mario_texture);
+            sm64_global_init(romBuffer, marioTexture);
             sm64_audio_init(romBuffer);
 
             //for(int i=0; i<3*SM64_GEO_MAX_TRIANGLES; i++) mario_indices[i] = i;
             delete[] romBuffer;
+
+            for (int i=0; i<SM64_TEXTURE_WIDTH * SM64_TEXTURE_HEIGHT; i++)
+            {
+                // swap red and blue colors
+                uint8_t r = marioTexture[i*4+0];
+                marioTexture[i*4+0] = marioTexture[i*4+2];
+                marioTexture[i*4+2] = r;
+            }
 
             audio_thread_init();
             sm64_play_sound_global(SOUND_MENU_STAR_SOUND);
 
             loaded = true;
             message = "libsm64 loaded";
+
+            initD3D();
         }
+    }
+
+    static void destroy()
+    {
+        if (!loaded) return;
+
+        audio_thread_stop();
+        sm64_global_terminate();
+        destroyD3D();
+        delete[] marioTexture;
+
+        loaded = false;
     }
 
     static void tick()
@@ -63,7 +90,14 @@ public:
     }
 
     sm64_san_andreas() {
-        Events::initGameEvent.Add(init);
+        loaded = false;
+        marioTexture = nullptr;
+
+        Events::initRwEvent.Add(init);
+        Events::shutdownRwEvent.Add(destroy);
+        Events::d3dLostEvent.Add(destroyD3D);
+        Events::d3dResetEvent.Add(initD3D);
         Events::gameProcessEvent.Add(tick);
+        Events::drawingEvent.Add(draw);
     }
 } _sm64_san_andreas;
