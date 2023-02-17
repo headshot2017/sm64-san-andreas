4f38d16f990c03e1889f728fb0f814fbb9b9de19
headshot2017 <nou@jm>
2023-02-16 21:38:35 -0400

    increase floor/ceil limits even more
    so that mario can move around when indoors

--- libsm64/src/decomp/engine/surface_collision.h
+++ libsm64/src/decomp/engine/surface_collision.h
@@ -7,10 +7,10 @@
 #include "../include/types.h"
 
 #define LEVEL_BOUNDARY_MAX  0x2000
-#define CELL_SIZE           0x400       
+#define CELL_SIZE           0x400
 
-#define CELL_HEIGHT_LIMIT   100000.f
-#define FLOOR_LOWER_LIMIT  -110000.f
+#define CELL_HEIGHT_LIMIT   500000.f
+#define FLOOR_LOWER_LIMIT  -500000.f
 
 s32 f32_find_wall_collision(f32 *xPtr, f32 *yPtr, f32 *zPtr, f32 offsetY, f32 radius);
 s32 find_wall_collisions(struct SM64WallCollisionData *colData);
