bc75c2bcb84727dcd2794de7bd0479652a6d2b48
headshot2017 <nou@jm>
2023-03-05 03:03:52 -0400

    libsm64: common idle cancels in act_first_person

--- libsm64/src/decomp/game/mario_actions_stationary.c
+++ libsm64/src/decomp/game/mario_actions_stationary.c
@@ -1075,6 +1075,10 @@ s32 act_first_person(struct MarioState *m) {
     }
 */
 
+    if (check_common_idle_cancels(m)) {
+        return TRUE;
+    }
+
 //  if (m->floor->type == SURFACE_LOOK_UP_WARP
 //      && save_file_get_total_star_count(gCurrSaveFileNum - 1, COURSE_MIN - 1, COURSE_MAX - 1) >= 10) {
 //      s16 sp1A = m->statusForCamera->headRotation[0];
